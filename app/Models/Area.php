e35d1c1ca3cc9e8215f43b379e559c712532e3bc
phongngo2002 <phongcuoi2213@gmail.com>
2022-10-15 21:52:52 +0700

    update

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class Area extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'areas';
 
